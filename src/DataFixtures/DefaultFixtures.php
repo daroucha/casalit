93fe8878b1be9905b6ecdc7fbb495ce1c945e05c
Aroucha Bros Pinto <arouchabrospinto@imac-de-aroucha.home>
2020-02-11 12:45:31 -0300

    1.0.0

--- src/DataFixtures/DefaultFixtures.php
+++ src/DataFixtures/DefaultFixtures.php
@@ -5,15 +5,26 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class DefaultFixtures extends Fixture
 {
+  private $passwordEncoder;
+
+  public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+  {
+    $this->passwordEncoder = $passwordEncoder;
+  }
+
   public function load(ObjectManager $manager)
   {
     $user = new User();
-
+    
     $user->setUsername('admin');
-    $user->setPassword('casalit_1234');
+    $user->setPassword($this->passwordEncoder->encodePassword(
+      $user,
+      'casalit_1234'
+    ));
 
     $manager->persist($user);
 
